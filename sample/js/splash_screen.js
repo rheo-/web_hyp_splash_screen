b732e3b63b567776a44f67cd256bc3796eec863b
Shahriar Kabir <a.toxophilite@gmail.com>
2013-10-28 18:15:08 +0600

    Update splash_screen.js

--- sample/js/splash_screen.js
+++ sample/js/splash_screen.js
@@ -7,10 +7,8 @@ ponir(dot)bd(at)hotmail(dot)com
 */
 
 $(document).ready( function(){
-
 	$("#hyp_splash_terminate").on("click", function(){
-		$("#hyp_splash_screen").toggle(1000);
-		$("html").css("overflow", "visible");
+		$("#hyp_splash_screen").hide(700);
 		return false;
 	});
 });
